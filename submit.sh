1a8cec6ff2d2113b51a40faa3b7547da394efa90
Marius Lange <marius.lange@bsse.ethz.ch>
2025-04-09 16:21:35 +0200

    Clean up

--- submit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#SBATCH --partition           test
-#SBATCH --time                01:00:00
-
-module load test_module
-source $HOME/.bashrc
-mamba activate test_env
-wandb agent "test_entity/test_project/test-sweep-id"
