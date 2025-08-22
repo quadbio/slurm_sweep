146fa4aa50ad0cc34dd3fb076c2e8e582e4b4252
Marius1311 <marius.lange@bsse.ethz.ch>
2025-08-22 13:45:24 +0200

    Update configs and notebook

--- examples/single_cell_embedding/submit.sh
+++ examples/single_cell_embedding/submit.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-#SBATCH --array               0-8
-#SBATCH --job-name            scembed_cpu
-#SBATCH --mem-per-cpu         32G
-#SBATCH --output              slurm_logs/cpu_%A_%a.out
+#SBATCH --array               0-15
+#SBATCH --gpus                rtx_4090:1
+#SBATCH --job-name            scembed_gpu
+#SBATCH --mem-per-cpu         16G
+#SBATCH --output              slurm_logs/gpu_%A_%a.out
 #SBATCH --time                01:00:00
 
 module load stack eth_proxy
 source $HOME/.bashrc
-mamba activate slurm_sweep
-wandb agent "spatial_vi/scembed_test/a75cfyzf"
+mamba activate scembed
+wandb agent "spatial_vi/scembed_test_2/ahaao6mt"
