1f4107d657bdc0390a2230b0b7a8a43fab72ea67
Marius1311 <marius.lange@bsse.ethz.ch>
2025-08-29 14:07:08 +0200

    Update configs and visualize sweep results

--- examples/spatial_embedding/submit.sh
+++ examples/spatial_embedding/submit.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-#SBATCH --array               0-3
-#SBATCH --gpus                rtx_4090:1
-#SBATCH --job-name            scembed_resolvi
+#SBATCH --job-name            scembed_pca
 #SBATCH --mem-per-cpu         32G
-#SBATCH --output              slurm_logs/resolvi_%A_%a.out
+#SBATCH --output              slurm_logs/pca_%A_%a.out
 #SBATCH --time                01:00:00
 
 module load stack eth_proxy
 source $HOME/.bashrc
-mamba activate scembed
-wandb agent --count 1 "spatial_vi/scembed_test_spatial_rc1/o8jzlhkp"
+mamba activate slurm_sweep
+wandb agent "spatial_vi/scembed_test_spatial_2/oreacgyh"
